8c91c052cc6905a547cbb59de70ec9025964e7e1
Xander Bass <xander_bass@mail.ru>
2022-03-22 11:09:08 +0400

    Frontend

--- www/chunks/head.php
+++ www/chunks/head.php
@@ -3,11 +3,12 @@
 
     if (empty($title))       $title       = '';
     if (empty($description)) $description = '';
+    $title = 'Rosh'.(empty($title) ? '' : ' | '.$title);
 ?>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
 <meta name="format-detection" content="telephone=no">
-<title><?=(empty($title) ? '' : ' | '.$title)?></title>
+<title><?=$title?></title>
 <meta name="description" content="<?=$description?>">
 <meta property="og:url" content="<?=Request::canonical()?>">
 <meta property="og:type" content="website">
